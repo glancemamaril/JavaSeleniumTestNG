bb01b39bdc0d48da2827ee44ae407841d7be83c8
glancemamaril <142483217+glancemamaril@users.noreply.github.com>
2023-12-23 23:27:53 +0800

    Update LC_MainPage.java

--- src/main/java/pageObjects/LegendCup/LC_MainPage.java
+++ src/main/java/pageObjects/LegendCup/LC_MainPage.java
@@ -1,7 +1,23 @@
 package pageObjects.LegendCup;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 import userHelper.UserHelper;
 
 public class LC_MainPage extends UserHelper{
 
+	WebDriver driver;
+	private String desc;
+	private String methodName;
+	
+	//Top Navigation
+	private final By ddGames = By.xpath("//button[contains(text(),'Games')]");
+	private final By ddSocial = By.xpath("//button[contains(text(),'Social')]");
+	private final By ddShrine = By.xpath("//button[contains(text(),'Shrine')]");
+	private final By ddSearch = By.xpath("//button[contains(text(),'Shrine')]");
+	
+	public LC_MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
